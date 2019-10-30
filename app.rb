467126f97e536ad146b6853979134cff7dd51231
Ed Courage <edcourage@live.com>
2019-10-30 16:09:11 +0000

    Added simple HP for player 2

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-
+enable :sessions
   get '/' do
     erb :index
   end
 
   post '/players' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect "/play"
+
+  end
+
+  get '/play'do
+    @player_1_name = session[:player_1]
+    @player_2_name = session[:player_2]
     erb :play
   end
 end
--- spec/features/test_infrastructure_spec.rb
+++ spec/features/test_infrastructure_spec.rb
@@ -2,25 +2,23 @@
 
 feature "Local output tests" do
 
+before :each do
+  visit("/")
+  fill_in :player_1, with: "Ed"
+  fill_in :player_2, with: "Topher"
+  click_on 'Submit'
+end
 
 
-  context "name - forms" do
-
-    scenario "if you input your names into the forms
-    will it print them to page" do
-      visit("/")
-      fill_in :player_1, with: "Ed"
-      fill_in :player_2, with: "Topher"
-
-      click_on 'Submit'
-
-      expect(page).to have_text("Ed vs Topher")
-
-
-    end
-
-
+context "display" do
+  scenario "if you input your names into the forms
+  will it print them to page" do
+    expect(page).to have_text("Ed vs Topher")
   end
 
+  scenario "player 2 HP" do
+    expect(page).to have_text("Topher: 100HP")
+  end
+end
 
 end
