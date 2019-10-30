0a57f83bcc77c311c6764eaf40e4d02c7ad0746d
Ed Courage <edcourage@live.com>
2019-10-30 15:16:38 +0000

    You can now enter player names

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    "Hello Battle!"
+    erb :index
   end
 
+  post '/players' do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb :play
+  end
 end
--- spec/features/test_infrastructure_spec.rb
+++ spec/features/test_infrastructure_spec.rb
@@ -1,8 +1,26 @@
 
 
-feature "Battle" do
-  scenario "does page return Hello Battle!" do
-    visit("/")
-    expect(page).to have_text("Hello Battle!")
+feature "Local output tests" do
+
+
+
+  context "name - forms" do
+
+    scenario "if you input your names into the forms
+    will it print them to page" do
+      visit("/")
+      fill_in :player_1, with: "Ed"
+      fill_in :player_2, with: "Topher"
+
+      click_on 'Submit'
+
+      expect(page).to have_text("Ed vs Topher")
+
+
+    end
+
+
   end
+
+
 end
